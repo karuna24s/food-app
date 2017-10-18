9da0553db3edd786c42d370e79cf96e9422e3692
karuna24s <karuna24s@gmail.com>
2017-10-18 16:19:21 -0400

    remove comments from CartItem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,32 +3,21 @@ import React from "react";
 class CartItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state= {
       itemTitle : this.props.itemTitle,
       price : this.props.price
     }
   }
   render() {
     return (
-      /*<div className="row">*/
-        <div className="col-sm-6 col-md-4">
+        <div className="row">
           <div className="thumbnail">
-            {/*<img src="..." alt="...">*/}
             <div className="caption">
               <h3>{this.props.itemTitle}</h3>
               <h4>Cost: {this.props.price}</h4>
-              {/*<p>
-                <a href="#" class="btn btn-primary" role="button">
-                  Button
-                </a>{" "}
-                <a href="#" class="btn btn-default" role="button">
-                  Button
-                </a>
-              </p>*/}
             </div>
           </div>
         </div>
-      /*</div>*/
     );
   }
 }
